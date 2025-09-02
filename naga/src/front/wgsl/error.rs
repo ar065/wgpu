d3e30c613b7335cf7ba3b11cea25bbab263d10eb
ar065 <45273698+ar065@users.noreply.github.com>
2025-09-02 19:48:04 -0400

    Update error.rs

--- naga/src/front/wgsl/error.rs
+++ naga/src/front/wgsl/error.rs
@@ -109,8 +109,11 @@ impl ParseError {
         let config = term::Config::default();
 
         let mut writer = crate::error::DiagnosticBuffer::new();
-        term::emit(writer.inner_mut(), &config, &files, &self.diagnostic())
-            .expect("cannot write error");
+        {
+            let mut no_color = termcolor::NoColor::new(writer.inner_mut());
+            term::emit(&mut no_color, &config, &files, &self.diagnostic())
+        }
+        .expect("cannot write error");
         writer.into_string()
     }
 
